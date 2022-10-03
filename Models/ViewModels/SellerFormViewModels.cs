4f5ec4c0e1774b9933e75a8922caf9f9a8bd8d88
Rafael Muzel <rafaelmuzel@yahoo.com.br>
2022-10-03 17:35:03 +0100

    SellerFormViewModel and Department select component

---
+++ Models/ViewModels/SellerFormViewModels.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class SellerFormViewModels
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
